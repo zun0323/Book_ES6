0a79135bc7bbf91f79abbff93c01b2a9c211d47f
Ryan <zun0323@gmail.com>
2019-03-24 00:48:33 +0900

    setup Views

--- app.js
+++ app.js
@@ -11,4 +11,9 @@ app.use(cookiePaser());
 app.use(bodyPaser.json());
 app.use(bodyPaser.urlencoded({ extended: true }));
 
+app.set("view engine", "pug");
+app.set("views", "./views");
+
+app.get("/", (req, res) => res.render("home"));
+
 export default app;
